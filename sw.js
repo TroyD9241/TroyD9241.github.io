9b437e85621404e7f14c7d7d21988e16ed10783f
Troy <troyd9216@gmail.com>
2021-06-22 08:33:03 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-aa4e0e4ccb623325504b.js"
+    "url": "webpack-runtime-b4d20fe2d96087ec6ec6.js"
   },
   {
     "url": "framework-868c53fff4771b0bde98.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "402fcf8a6e89fd1aaebd307ac1c2ad16"
+    "revision": "c1bbe1ff93999742950009d37f3f82ac"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
